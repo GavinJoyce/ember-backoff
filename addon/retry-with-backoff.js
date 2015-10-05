818598d56db30efeef2e9c548ec1a55fd8fb276e
Linus Oleander <linus@oleander.nu>
2015-10-06 01:27:40 +0200

    Pass 'abort' to callback

--- addon/retry-with-backoff.js
+++ addon/retry-with-backoff.js
@@ -1,23 +1,39 @@
 import Em from 'ember';
 import delay from 'ember-delay/delay';
 
-var retryWithBackoff = function(callback, retryCountBeforeFailure, waitInMilliseconds) {
-  if(Em.isEmpty(retryCountBeforeFailure)) {
-    retryCountBeforeFailure = 5;
-  }
-  waitInMilliseconds = waitInMilliseconds || 250;
-
-  return callback().catch(function(reason) {
+var retryWithBackoff = function(callback, retryCountBeforeFailure, waitInMilliseconds, resolve, reject) {
+  callback(reject).then(resolve).catch(function(reason) {
     if (retryCountBeforeFailure-- > 1) {
       waitInMilliseconds = waitInMilliseconds * 2;
 
       return delay(waitInMilliseconds).then(function() {
-        return retryWithBackoff(callback, retryCountBeforeFailure, waitInMilliseconds);
+        return retryWithBackoff(
+          callback, 
+          retryCountBeforeFailure, 
+          waitInMilliseconds, 
+          resolve, 
+          reject
+        );
       });
     }
 
-    throw reason;
+    reject(reason);
   });
 };
 
-export default retryWithBackoff;
+export default function(callback, retryCountBeforeFailure, waitInMilliseconds){
+  if(Em.isEmpty(retryCountBeforeFailure)) {
+    retryCountBeforeFailure = 5;
+  }
+  waitInMilliseconds = waitInMilliseconds || 250;
+  
+  return new Em.RSVP.Promise(function(resolve, reject) {
+    retryWithBackoff(
+      callback, 
+      retryCountBeforeFailure, 
+      waitInMilliseconds, 
+      resolve, 
+      reject
+    );
+  });
+};
