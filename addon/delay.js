dff7af6416c961bb12fea8e9ae0b9b23d3b2d434
Mandy Hegarty <mandy.hegarty@intercom.io>
2024-01-22 12:09:44 +0000

    import later directly

--- addon/delay.js
+++ addon/delay.js
@@ -1,11 +1,12 @@
-import Em from 'ember';
+import Em from "ember";
+import { later } from "@ember/runloop";
 
-export default function(milliseconds) {
-  if(milliseconds === undefined) {
+export default function (milliseconds) {
+  if (milliseconds === undefined) {
     milliseconds = 2000;
   }
 
-  return new Em.RSVP.Promise(function(resolve) {
-    Em.run.later(this, resolve, milliseconds);
+  return new Em.RSVP.Promise(function (resolve) {
+    later(this, resolve, milliseconds);
   });
 }
